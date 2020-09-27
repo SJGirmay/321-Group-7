dd94dbc16ddd26159dec7448fb1899ec44109939
SJGirmay <71671587+SJGirmay@users.noreply.github.com>
2020-09-27 17:46:58 -0400

    Delete CovidLocator.java
    Moved to proper folder

--- CovidLocator.java
+++
@@ -1,6 +0,0 @@
-public class CovidLocator {
-    public static void main(String[] args) {
-        System.out.println("This is the code for the Covid Tester!");
-        System.out.println("Hello World!");
-    }
- }
