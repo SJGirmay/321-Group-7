8afa57768a1749b93f23b7541b4fb758faa8c586
Shumjuna Girmay <LightLifter@github.com>
2020-12-10 22:25:57 -0500

    No country option

--- Covid-Locator/NationalMenu.py
+++ Covid-Locator/NationalMenu.py
@@ -2,9 +2,33 @@ import pip._vendor.requests
 import json
 from Parser import jprint
 #This Data comes from Worldometers
-natURL = "https://disease.sh/v3/covid-19/countries/USA"
+natURL = "https://disease.sh/v3/covid-19/countries/USA?yesterday=true&twoDaysAgo=true&strict=true"
 def NationalMenu():
     response = pip._vendor.requests.get(natURL)
     jprint(response.json())
 
-#NationalMenu()
+NationalMenu()
+
+
+#"flag": "https://disease.sh/assets/img/flags/us.png", Keep this
+#int active
+#int activePerOneMillion
+#int cases
+#int casesPerOneMillion
+#int countryInfo 
+#int critical
+#int criticalPerOneMillion
+#int deaths
+#int deathsPerOneMillion 
+#int oneCasePerPeople 
+#int oneDeathPerPeople
+#int oneTestPerPeople 
+#int population  
+#int recovered 
+#int recoveredPerOneMillion  
+#int tests 
+#int testsPerOneMillion 
+#int todayCases 
+#int todayDeaths 
+#int todayRecovered 
+#int updated 
