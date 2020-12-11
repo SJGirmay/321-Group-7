a860a285d317eb2ca894112dca35053ce01d5b45
Shumjuna Girmay <LightLifter@github.com>
2020-12-11 18:47:53 -0500

    Tons of edits

--- Covid-Locator/NationalMenu.py
+++ Covid-Locator/NationalMenu.py
@@ -1,6 +1,8 @@
 import pip._vendor.requests
 import json
 from Parser import jprint,nationalDataPrint
+from flask import Flask, render_template, request
+
 #This Data comes from Worldometers
 natURL = "https://disease.sh/v3/covid-19/countries/USA?yesterday=true&twoDaysAgo=true&strict=true"
 #"https://disease.sh/assets/img/flags/us.png", Keep this for HTML
@@ -8,3 +10,4 @@ def NationalMenu():
     response = pip._vendor.requests.get(natURL)
     nationalDataPrint(response.json())
 
+index = open("nationalPage.html").read().format(first_header='goodbye', p1='World', p2='Hello')
